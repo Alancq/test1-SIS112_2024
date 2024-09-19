8625bd5abf93d82084957ada2d9a5781c5edf49f
Melissa-sis112 <ibemaguzmeli.17@gmail.com>
2024-09-19 18:48:09 -0400

    prototipo del examen

---
+++ Melissa_Ortega/Prototipo_Exam.py
@@ -0,0 +1,12 @@
+def ingresar_matriz():
+    ingrese_valores=int(input("Ingrese los valores de la matriz: "))
+    matriz = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+    for i in range(matriz):
+        ingrese_valores.apped(matriz)
+def suma_total(matriz):
+    sum(matriz)
+fun.
---
+++ Melissa_Ortega/funciones.py
@@ -0,0 +1,4 @@
+def ingresar_matriz():
+    ingrese_valores=int(input("Ingrese los valores de la matriz: "))
+def suma_total(matriz):
+    print("no puedo")
