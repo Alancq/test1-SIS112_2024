41a8f31733f2e99dff8882cb8153b2587200e16a
Andres27Gallardo <andres.gallardo@ucb.edu.bo>
2024-09-19 12:25:25 -0400

    Actu_pilo_v2

--- ANDRES_GALLARDO/PRUEB_EXA_2.py
+++ ANDRES_GALLARDO/PRUEB_EXA_2.py
@@ -1,19 +1,32 @@
 matrizMostr1 = []
 
 for i in range (9):
-    numUsuario1 = int(input(f"Ingrese el {i+1} ° numero de la matriz: "))
+    numUsuario1 = int(input(f"Ingrese el {i+1}° numero de la matriz: "))
     matrizMostr1.append(numUsuario1)
     
 totalMatriz = sum(matrizMostr1)
 maxMatriz = max(matrizMostr1)
 minMatriz = min(matrizMostr1)
 
-print (matrizMostr1)
+sumaFila1 = (matrizMostr1[0]+matrizMostr1[3]+matrizMostr1[6])
+sumaFila2 = (matrizMostr1[1]+matrizMostr1[4]+matrizMostr1[7])
+sumaFila3 = (matrizMostr1[2]+matrizMostr1[5]+matrizMostr1[8])
+
+sumaColum1 = (matrizMostr1[0]+matrizMostr1[1]+matrizMostr1[2])
+sumaColum2 = (matrizMostr1[3]+matrizMostr1[4]+matrizMostr1[5])
+sumaColum3 = (matrizMostr1[6]+matrizMostr1[7]+matrizMostr1[8])
+
+print("Matriz 3x3: ")
+print (matrizMostr1[0], matrizMostr1[3], matrizMostr1[6])
+print (matrizMostr1[1], matrizMostr1[4], matrizMostr1[7])
+print (matrizMostr1[2], matrizMostr1[5], matrizMostr1[8])
 
 print ("Sumatoria de la las matriz: ", totalMatriz)
 print ("El maximo de la matriz es: ", maxMatriz)
 print ("El minimos de la matriz es: ", minMatriz)
-    
-
-
-    
+print ("Suma de la primera fila: ", sumaFila1)
+print ("Suma de la segunda fila: ", sumaFila2)
+print ("Suma de la tercera fila: ", sumaFila3)
+print ("Suma de la primera columna: ",sumaColum1)
+print ("Suma de la primera columna: ",sumaColum2)
+print ("Suma de la primera columna: ",sumaColum3)
